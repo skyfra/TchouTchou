1e48fe79dd4a238772f4e7fbb0ed955e534c1d95
Kylian <admin@skyfra.studio>
2021-06-04 18:40:20 +0200

    Mise à jour majeur !
    - Vérification de si le package (sl) est installé
    - Vérification de si l'utilisateur est root pour installer le package automatiquement
    
    mais si le package n'est pas install il retourne un petit message

--- tchoutchou.sh
+++ tchoutchou.sh
@@ -1,6 +1,22 @@
 echo "Installation des dépendances ..."
 
 wait 1
+
+if ! [ -x "$(command -v sl)" ]; then
+  echo "* le package sl requis pour faire fonctionner ce script"
+  echo "* utilise apt install sl pour installer la train tchoutchou"
+  
+  if [[ $EUID -ne 0 ]]; then
+    echo "* Tu dois avoir accés au root afin d'installer le module" 1>&2
+    exit 1
+  fi
+  
+  apt install sl -y
+  
+  exit 1
+fi
+
+
 apt update
 apt install sl -y
 
