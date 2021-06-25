f005d9cb865d7867f8b21189519eafb2e2cc9eb9
Kylian <admin@skyfra.studio>
2021-06-25 23:38:50 +0200

    [Fix + Correction] - SkyFr
    Fix de fautes, rajout d'un message d'introduction et vérification de si le packet est installé (supprimé dans la dernière PR) mais extrement utile si le package n'est pas installé ce qui pourrait donc spammer d'erreurs dans la console

--- tchoutchou.sh
+++ tchoutchou.sh
@@ -1,17 +1,27 @@
-echo "Installation des dépendances ..."
+echo "Bienvenue dans tchoutchou le tout premier script ferroviaire de github" 
+echo "Nous allons avant tout vérifier que tchoutchou peut se lancer sur ta machine..." 
 
-sleep 2
+if ! [ -x "$(command -v sl)" ]; then
+  echo "* le package sl requis pour faire fonctionner ce script"
+  echo "* utilise apt install sl pour installer tchoutchou"
 
-apt update
-apt install sl -y
+  if [[ $EUID -ne 0 ]]; then
+    echo "* Tu dois avoir accés au root afin d'installer le module" 1>&2
+    exit 1
+  fi
+
+  apt install sl -y
+
+  exit 1
+fi
 
 clear
 
-echo "! une fois le train lancé, enfoncée CTRL+C pendant quelque seconde pour stoper le train !"
+echo "! une fois le train lancé, enfoncez CTRL+C pendant quelque seconde pour stoper le train !"
 read -p "appuyer sur ENTRER pour lancer le train"
 
 clear
-echo "Lancement de TchouTchou.sh (édition 2021)"
+echo "Lancement de TchouTchou.sh (édition summer 2021)"
 sleep 1
 
 for (( ; ; ))
