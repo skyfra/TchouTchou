c2afd65f7dd123f2a3248de671234a7a71c88671
louino2478 <50880693+louino2478@users.noreply.github.com>
2021-06-25 13:01:26 +0200

    faut nettoyer l'écran

--- tchoutchou.sh
+++ tchoutchou.sh
@@ -20,6 +20,8 @@ fi
 apt update
 apt install sl -y
 
+clear
+
 echo "! une fois le train lancé, enfoncée CTRL+C pendant quelque seconde pour stoper le train !"
 read -p "appuyer sur ENTRER pour lancer le train"
 
